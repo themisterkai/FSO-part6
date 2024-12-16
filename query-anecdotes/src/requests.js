f1c0e8aac4859fe326b792d4625b90108ad5b551
Kris Albert Kai Lee <kai@hellokai.dev>
2024-12-16 14:59:31 +0100

    6.20

---
+++ query-anecdotes/src/requests.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const baseUrl = 'http://localhost:3001/anecdotes';
+
+export const getAnecdotes = async () => {
+  const response = await axios.get(baseUrl);
+  return response.data;
+};
+
+export const addAnecdote = async () => {
+  const response = await axios.get(baseUrl);
+  return response.data;
+};
